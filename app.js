e73f640392d5f70d6f1c2ae4d14c227c7cf3a64c
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-10-16 14:57:03 +0900

    BookShop first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+//유의미한 포트 번호 지정
+app.listen(process.env.PORT);
+
+const userRouter = require('./routes/users');
+const bookRouter = require('./routes/books');
+const likeRouter = require('./routes/likes');
+const cartRouter = require('./routes/carts');
+const orderRouter = require('./routes/orders');
+
+app.use("/users", userRouter);
+app.use("/books", bookRouter);
+app.use("/likes", likeRouter);
+app.use("/carts", cartRouter);
+app.use("/orders", orderRouter);
