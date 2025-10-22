d87128a93b1e086e24b016615b2a1a3734410182
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-10-22 16:44:02 +0900

    bookController.js sql문 수정 + 좋아요 API 추가

--- controller/bookController.js
+++ controller/bookController.js
@@ -7,7 +7,7 @@ const allBooks = (req, res) => {
     // limit : page 당 도서 수 , currentPage : 현재 페이지의 번호
     // offset : limit * (currentPage-1)
     let offset = limit * (currentPage-1);
-    let sql = "SELECT * FROM books";
+    let sql = "SELECT *, (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes FROM books";
     let values = [];
     if (category_id && news) {
         sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();";
@@ -38,10 +38,18 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-    let { id } = req.params;
+    let book_id = req.params.id;
+    let {user_id} = req.body;
 
-    let sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id = 1;`;
-    conn.query(sql, id,
+    let sql = `SELECT *,
+	                    (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes,
+	                    (SELECT EXISTS(SELECT * FROM likes WHERE user_id = ? AND liked_book_id = ?)) AS liked
+                    FROM books
+                    LEFT JOIN category
+                    ON books.category_id = category.category_id
+                    WHERE books.id = ?;`;
+    let values = [user_id, book_id, book_id]
+    conn.query(sql, values,
         (err, results) => {
             if (err) {
                 console.log(err);
